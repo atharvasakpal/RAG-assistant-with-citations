992bb7e8e90155438cc4fbce3917ee81194534bb
Atharva Sakpal <40302268+atharvasakpal@users.noreply.github.com>
2025-12-29 22:53:03 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-GOOGLE_API_KEY = "AIzaSyATvOSTGTLgG0TF5mIWU61LkPXq3pYYdT4"
-
-
-def set_environment():
-    variable_dict = globals().items()
-    for key, value in variable_dict:
-        if "API" in key or 'ID' in key:
-            os.environ[key] = value
